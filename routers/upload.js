58cff6cf8227521f8796a83a329cb830a96783f3
jaimiederijk <j.r.derijk@quicknet.nl>
2015-01-29 14:26:24 +0100

    iets geen idee

--- routers/upload.js
+++ routers/upload.js
@@ -58,6 +58,7 @@ router.post('/', function (req, res, next){
 
 });
 router.get('/showuploads', function (req, res){
+
 	
 	fs.readdir(filesPath, function (err, files){
 		if (err){
@@ -67,8 +68,29 @@ router.get('/showuploads', function (req, res){
 			files: files
 		});
 	});
-	
-
-
+	console.log(req.body.file)
 });
+router.post("/showuploads",function(req,res){
+	photoId;
+	date=new date();
+	req.getConnection(function(err, connection){
+		if(err){ next(err); }
+
+		connection.query("SELECT id FROM photos WHERE id=(?)", [req.session.username], function(err, records){
+			if(err){ next(err); }
+
+			photoId =   records[0].id;
+		});
+	});
+
+	req.getConnection(function(err, connection){
+		if(err){ next(err); }
+
+		connection.query("INSERT INTO comments (photo_id, created_at ,comment) VALUES (?)", [[photoId,date,req.body.comment]], function(err, records){
+		if(err){ next(err); }
+
+
+
+	});
+})
 module.exports = router;
